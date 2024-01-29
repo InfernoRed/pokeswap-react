b31d26e1217e88e38e43f3a278be47423fcd9ef7
Ella <ella.seeman@infernored.com>
2024-01-29 11:54:09 -0600

    Add more animations and change around layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import { useTranslation } from "react-i18next";
 import { usePokemonCardContext } from "store/pokemonCardProvider";
 import PokemonCardSelector from "~/shared/components/PokemonCardSelector";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 export const meta: MetaFunction = () => {
   return [
@@ -37,34 +37,40 @@ const letterVariants = {
 
 export default function Index() {
   const { t } = useTranslation();
-  const { pokemonCards } = usePokemonCardContext();
-  
+  const { pokemonCards, yourPokemonCards } = usePokemonCardContext();
+
   return (
     <div id="pokedex" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <div id="bar">
         <div id="buttonGlass">
-          <div id="reflect"/>
+          <div id="reflect" />
         </div>
-        <div id="miniButtonGlass1"/>
-        <div id="miniButtonGlass2"/>
-        <div id="miniButtonGlass3"/>
+        <div id="miniButtonGlass1" />
+        <div id="miniButtonGlass2" />
+        <div id="miniButtonGlass3" />
         {/* TODO: Need to find IRT logo */}
       </div>
-      <div id="screen" style={{ fontFamily: 'Pixer'}}>
-        <motion.div variants={sentenceVariants} initial={'hidden'} animate='visible'>
-          <motion.h1 className="text-2xl" >{
-            t("greeting").split("").map((letter, index) => {
+      <div style={{ fontFamily: 'Pixer' }} className="flex flex-row w-4/5 self-center justify-between items-center">
+        <motion.div variants={sentenceVariants} initial={'hidden'} animate='visible' >
+          <motion.h1 className="text-2xl text-white" >{
+            (t("build-deck-cta")).split("").map((letter, index) => {
               return <motion.span key={letter + index} variants={letterVariants} custom={index + 1}>{letter}</motion.span>
             })
           }</motion.h1>
-          <motion.h3 className="text-xl">{t("compare-pokemon-cta").split("").map((letter, index) => {
-              return <motion.span key={letter + index} variants={letterVariants} custom={index + 1}>{letter}</motion.span>
-            })}</motion.h3>
         </motion.div>
-        {pokemonCards && 
-          <div className="flex flex-row gap-8 justify-center mx-4 mt-4">
-            <PokemonCardSelector/>
-            <PokemonCardSelector/>
+        <div className="flex flex-row gap-2 items-center">
+          <div className="border-dashed border-black w-10 h-14 border-2 rounded-sm relative">
+            {yourPokemonCards.length > 0 && <motion.img src="https://images.pokemontcg.io/hgss4/0.png" className="absolute  h-full w-full top-0" initial={{ x: 100, opacity: 0 }} animate={{ x: -1 * yourPokemonCards.length, y: -1 * yourPokemonCards.length, opacity: 1 }} transition={{ duration: .5 }} style={{ boxShadow: `${yourPokemonCards.length}px ${yourPokemonCards.length}px rgb(27, 39, 80)` }} />}
+            {yourPokemonCards.length > 1 && <motion.img src="https://images.pokemontcg.io/hgss4/0.png" className="absolute  h-full w-full top-0" key={yourPokemonCards.length} initial={{ x: 100, opacity: 0 }} animate={{ x: -1 * yourPokemonCards.length, y: -1 * yourPokemonCards.length, opacity: 1 }} transition={{ duration: .5 }} />}
+          </div>
+          <p>{yourPokemonCards.length}</p>
+        </div>
+      </div>
+      <div id="screen" style={{ fontFamily: 'Pixer' }}>
+        {pokemonCards &&
+          <div className="flex flex-row gap-16 justify-center h-full">
+            <PokemonCardSelector />
+            <PokemonCardSelector />
           </div>
         }
       </div>
--- app/shared/components/PokemonCardSelector/index.tsx
+++ app/shared/components/PokemonCardSelector/index.tsx
@@ -1,42 +1,113 @@
 import { animate, motion } from "framer-motion";
 import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
-import { FC, useRef, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { usePokemonCardContext } from "store/pokemonCardProvider";
+import TypeIndicator from "../TypeIndicator";
 
 const animationDurationSeconds = .25;
 
 const PokemonCardSelector: FC = () => {
-    const { pokemonCards } = usePokemonCardContext();
-    const randomCardNumber = Math.floor(Math.random() * pokemonCards!.length);
-    const [pokemonCard, setPokemonCard] = useState<PokemonTCG.Card>(pokemonCards![randomCardNumber]);
-    const cardImageRef = useRef<HTMLDivElement>(null);
+  const { pokemonCards, addPokemonCard } = usePokemonCardContext();
+  const randomCardNumber = Math.floor(Math.random() * pokemonCards!.length);
+  const [pokemonCard, setPokemonCard] = useState<PokemonTCG.Card>(pokemonCards![randomCardNumber]);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const [pokemonHp, setPokemonHp] = useState<number>(0);
 
-    const onPokemonSelected = (e: React.ChangeEvent<HTMLSelectElement>) => {
-      const selectedCard = pokemonCards!.find((card) => card.id === e.target.value);
-      if(!selectedCard) return;
-      if(cardImageRef.current) {
-        animate(cardImageRef.current, {opacity: 0}, { duration: animationDurationSeconds, ease: 'easeInOut'});
+  const onPokemonSelected = () => {
+    const selectedCard = pokemonCards![Math.floor(Math.random() * pokemonCards!.length)];
+    if (!selectedCard) return;
+    if (cardRef.current) {
+      animate(cardRef.current, { opacity: 0 }, { duration: animationDurationSeconds, ease: 'easeInOut' });
+    }
+    //Better way to do this?
+    setTimeout(() => {
+      setPokemonCard(selectedCard);
+    }, animationDurationSeconds * 2000)
+  }
+
+  const pokemonDisplayVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: .75,
       }
-      //Better way to do this?
-      setTimeout(() => {
-        setPokemonCard(selectedCard);
-      }, animationDurationSeconds*1000)
+    }
+  }
+
+  const pokemonDataVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: .1,
+        staggerChildren: 1
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (!pokemonCard || !pokemonCard.hp) return;
+
+    const updateHP = () => {
+      setPokemonHp((prevState) => {
+        if (prevState >= parseInt(pokemonCard.hp)) {
+          clearInterval(intervalID);
+          return parseInt(pokemonCard.hp);
+        }
+        return prevState + 1
+      })
     }
 
-    return <div className="flex flex-col gap-2">
-      <select value={pokemonCard?.id} onChange={onPokemonSelected}>
-        {pokemonCards!.map((card) => {
-          return <option key={card.id} value={card.id}>{card.name}</option>
-        })}
-      </select>
-      <motion.div key={pokemonCard.id} className="w-64" 
-        ref={cardImageRef}
-        initial={{opacity: 0}} 
-        animate={{opacity: 1}} 
-        transition={{duration: animationDurationSeconds * 2, ease: 'easeOut'}}>
-          <img src={pokemonCard.images.small} alt={pokemonCard.name} className="w-full"/>
+    setPokemonHp(0);
+    const intervalID = setInterval(updateHP, 2000 / parseInt(pokemonCard.hp));
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [pokemonCard])
+
+  return <motion.div className="flex flex-col gap-2 justify-between w-0" style={{ "flex": "1 1 0" }} variants={pokemonDisplayVariants} initial={'hidden'} animate='visible' key={pokemonCard.name}>
+    <div className="flex flex-col justify-center gap-2" ref={cardRef}>
+      <motion.div className="flex justify-between" variants={pokemonDataVariants}>
+        <div className="flex flex-row items-center gap-2">
+          <h2 className="text-xl">{pokemonCard.name}</h2>
+          {pokemonCard.types?.map((type) => {
+            return <TypeIndicator key={type} type={type} />
+          })}
+        </div>
+        {pokemonCard.nationalPokedexNumbers && <h3>No. {pokemonCard.nationalPokedexNumbers[0]}</h3>}
+      </motion.div>
+      <motion.div className="flex flex-row gap-2 items-center" variants={pokemonDataVariants}>
+        <p className="font-bold">HP:</p>
+        <motion.div variants={pokemonDataVariants} className="h-4 bg-gray-700 border-solid border-black border-2" initial={{ width: '0px' }} animate={{ width: `${pokemonCard.hp}px` }} transition={{ delay: .5, duration: 2, ease: 'easeOut' }} />
+        <p className="text-center">{pokemonHp}</p>
+      </motion.div>
+      <motion.div key={pokemonCard.id} className="w-64 self-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: animationDurationSeconds * 2, ease: 'easeOut' }}>
+        <img src={pokemonCard.images.small} alt={pokemonCard.name} className="w-full" />
+      </motion.div>
+      <motion.div variants={pokemonDataVariants}>
+        {typeof pokemonCard.cardmarket?.prices?.averageSellPrice !== 'undefined' && <p className="font-sans text-xl text-center">{new Intl.NumberFormat('en-EN', { style: 'currency', currency: 'USD' }).format(pokemonCard.cardmarket?.prices?.averageSellPrice)}</p>}
+        {pokemonCard.flavorText && <p className="leading-normal mt-2 px-4 text-justify">{pokemonCard.flavorText}</p>}
       </motion.div>
     </div>
+    <div>
+      <div className="flex flex-row justify-between">
+        <button className="border-gray-700 border-solid border-2 px-2" onClick={onPokemonSelected}>See a new pokemon</button>
+        <button className="border-gray-700 border-solid border-2 px-2" onClick={() => {
+          addPokemonCard(pokemonCard);
+          onPokemonSelected();
+        }}>Add to Deck</button>
+      </div>
+    </div>
+  </motion.div>
 }
 
 export default PokemonCardSelector;
